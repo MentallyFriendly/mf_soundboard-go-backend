4fa0234b7fe144b18cf6c3987f1a0c0b34d939b7
Justin Davidson <justindavidson23@gmail.com>
2018-08-15 14:17:25 +1000

    Add /config_example folder

---
+++ config_example/config_example.go
@@ -0,0 +1,10 @@
+// change to package config
+package configexample
+
+import "os"
+
+// SetVars func
+func SetVars() {
+	os.Setenv("DB_PARAMS", "user=admin password=password dbname=<database-name-here> host=db sslmode=disable")
+	// os.Setenv("DB_PARAMS", "user=admin password=password dbname=<database-name-here> host=db<refers to 'db' service in docker-compose file> sslmode=disable")
+}
