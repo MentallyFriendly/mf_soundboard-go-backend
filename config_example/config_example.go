44c390ad6b1d05acd73243e7c8b7b49ec070beed
Justin Davidson <justindavidson23@gmail.com>
2018-08-15 14:55:39 +1000

    Add README and updated config example file

--- config_example/config_example.go
+++ config_example/config_example.go
@@ -5,6 +5,6 @@ import "os"
 
 // SetVars func
 func SetVars() {
-	os.Setenv("DB_PARAMS", "user=admin password=password dbname=<database-name-here> host=db sslmode=disable")
+	os.Setenv("DB_PARAMS", "user=admin password=password dbname=mf_soundboard-v1 host=db sslmode=disable")
 	// os.Setenv("DB_PARAMS", "user=admin password=password dbname=<database-name-here> host=db<refers to 'db' service in docker-compose file> sslmode=disable")
 }
